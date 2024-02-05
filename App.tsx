492867c625d6383c3a708d3cdd4c61eac86d7ea0
Igor Wiepieski <iw.programacao@gmail.com>
2024-02-05 18:41:05 -0300

    Native Base e Themming

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import { NativeBaseProvider } from 'native-base';
+import { Loading } from '@components/Loading';
+import { THEME } from 'src/theme';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {!fontsLoaded ? (
+        <Text>Open up App.tsx to start working on your app!</Text>
+      ) : (
+        <Loading />
+      )}
+    </NativeBaseProvider>
+  );
+}
