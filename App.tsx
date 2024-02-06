560122e43edb794b464831fb000722f74294db13
Igor Wiepieski <iw.programacao@gmail.com>
2024-02-05 21:51:31 -0300

    Tela de SignIn

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import {
 import { NativeBaseProvider } from 'native-base';
 import { Loading } from '@components/Loading';
 import { THEME } from 'src/theme';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,11 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {!fontsLoaded ? (
-        <Text>Open up App.tsx to start working on your app!</Text>
-      ) : (
-        <Loading />
-      )}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,36 @@
+import { Center, Heading, Image, Text, VStack } from 'native-base';
+import BackgroundImg from '@assets/background.png';
+import LogoSvg from '@assets/logo.svg';
+import { Input } from '@components/Input';
+
+export function SignIn() {
+  return (
+    <VStack flex={1} bgColor="gray.700" px={10}>
+      <Image
+        source={BackgroundImg}
+        alt="Pessoas treinando"
+        resizeMode="contain"
+        position="absolute"
+      />
+      <Center my={24}>
+        <LogoSvg />
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e corpo!
+        </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
+
+        <Input
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input placeholder="Senha" secureTextEntry />
+      </Center>
+    </VStack>
+  );
+}
