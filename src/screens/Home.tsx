af58a14225db16b1f8d840f87c5207d9f7a385a4
Igor Wiepieski <iw.programacao@gmail.com>
2024-02-08 22:22:14 -0300

    add exercises groups and flatlist

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,36 @@
 import { ExerciseGroup } from '@components/ExerciseGroup';
 import { HomeHeader } from '@components/HomeHeader';
-import { Center, Text, VStack } from 'native-base';
+import { FlatList, HStack, VStack } from 'native-base';
+import { useState } from 'react';
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costas');
+  const [groups, setGroups] = useState([
+    'costas',
+    'ombros',
+    'bíceps',
+    'tríceps',
+  ]);
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <ExerciseGroup name="Costas" />
+
+      <FlatList
+        data={groups}
+        horizontal
+        showsVerticalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <ExerciseGroup
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+      />
     </VStack>
   );
 }
