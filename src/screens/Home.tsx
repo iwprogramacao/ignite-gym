4204dd5aef173ee4cbec815feb6fb2c79adf4c74
Igor Wiepieski <iw.programacao@gmail.com>
2024-02-09 14:37:30 -0300

    add complete home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,7 @@
+import { ExerciseCard } from '@components/ExerciseCard';
 import { ExerciseGroup } from '@components/ExerciseGroup';
 import { HomeHeader } from '@components/HomeHeader';
-import { FlatList, HStack, VStack } from 'native-base';
+import { FlatList, HStack, Heading, Text, VStack } from 'native-base';
 import { useState } from 'react';
 
 export function Home() {
@@ -11,6 +12,13 @@ export function Home() {
     'bíceps',
     'tríceps',
   ]);
+  const [exercicios, setExercicios] = useState([
+    'Puxada frontal',
+    'Remada Curvada',
+    'Remada unilateral',
+    'Levantamento terra',
+  ]);
+
   return (
     <VStack flex={1}>
       <HomeHeader />
@@ -31,6 +39,26 @@ export function Home() {
           />
         )}
       />
+
+      <VStack flex={1} px={8} mb={5}>
+        <HStack justifyContent="space-between">
+          <Heading color="gray.200" fontSize="md" fontFamily="heading">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            {exercicios.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercicios}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
     </VStack>
   );
 }
