820b8123240ac6d31f9a263277e51dd1bfec79e2
Igor Wiepieski <iw.programacao@gmail.com>
2024-02-08 21:04:41 -0300

    add header app component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,29 @@
+import { HStack, Heading, Icon, Text, VStack } from 'native-base';
+import { UserPhoto } from './UserPhoto';
+import { MaterialIcons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
+
+export function HomeHeader() {
+  return (
+    <HStack bgColor="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        size={16}
+        source={{ uri: 'https://github.com/iwprogramacao.png' }}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Igor Wiepieski
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
